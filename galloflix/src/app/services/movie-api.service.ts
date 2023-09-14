7f7a03cfc296f57b3c79c53268ed586dad5d8f4f
MatsudaEol <110265861+MatsudaEol@users.noreply.github.com>
2023-09-13 21:19:37 -0300

    Aula 1 :)

---
+++ galloflix/src/app/services/movie-api.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieApiService {
+
+  constructor(private http: HttpClient) { }
+
+  baseUrl = "https://api.themoviedb.org/3"
+  apiKey = "de6cc024e0d58505136a9d924a0c7d6f"
+
+  //banner API Data
+  bannerApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/all/week?api_key=${this.apiKey}&language=pt-BR`);
+  }
+
+}
